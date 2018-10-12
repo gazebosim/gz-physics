436ecb6df6892f8b042f2f39958165a6c616b955
Steve Peters <scpeters@openrobotics.org>
2018-10-12 16:23:50 -0700

    start moving DoublePendulum example to test folder and update API, disable SetState for now

---
+++ test/MockDoublePendulum.hh
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef IGNITION_PHYSICS_TEST_MOCKDOUBLEPENDULUM_HH_
+#define IGNITION_PHYSICS_TEST_MOCKDOUBLEPENDULUM_HH_
+
+#include <ignition/physics/FeatureList.hh>
+#include <ignition/physics/ForwardStep.hh>
+#include <ignition/physics/CanWriteData.hh>
+
+namespace mock
+{
+  class MockDoublePendulum
+      : public ignition::physics::CanWriteRequiredData<
+            MockDoublePendulum,
+            ignition::physics::ForwardStep::Output>
+  {
+    public: virtual void Write(ignition::physics::JointPositions &_positions) const = 0;
+
+    /// \brief Write poses for each link in inertial frame (at COM).
+    public: virtual void Write(ignition::physics::WorldPoses &_poses) const = 0;
+
+    public: virtual ~MockDoublePendulum() = default;
+  };
+
+  using MockDoublePendulumList = ignition::physics::FeatureList<
+    ignition::physics::ForwardStep
+  >;
+}
+
+#endif
