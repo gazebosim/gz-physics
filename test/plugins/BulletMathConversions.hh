c3816f0eeac80ea1a823a96d5962b96aa595105d
Steve Peters <scpeters@openrobotics.org>
2018-12-12 17:10:16 -0800

    whitespace: fix indent for BulletMathConversions.hh

--- test/plugins/BulletMathConversions.hh
+++ test/plugins/BulletMathConversions.hh
@@ -23,81 +23,81 @@
 
 namespace mock
 {
-    namespace bullet
+  namespace bullet
+  {
+    // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    // ---------------- Converting to Bullet ---------------------
+    inline btVector3 convert(const ignition::math::Vector3d &v)
     {
-      // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-      // ---------------- Converting to Bullet ---------------------
-      inline btVector3 convert(const ignition::math::Vector3d &v)
-      {
-        return btVector3(v[0], v[1], v[2]);
-      }
-
-      inline btMatrix3x3 convert(const ignition::math::Matrix3d &m)
-      {
-        btMatrix3x3 matrix;
-        for (std::size_t i=0; i < 3; ++i)
-          for (std::size_t j=0; j < 3; ++j)
-            matrix[i][j] = m(i, j);
-
-        return matrix;
-      }
-
-      inline btQuaternion convert(const ignition::math::Quaterniond &q)
-      {
-        return btQuaternion(q.X(), q.Y(), q.Z(), q.W());
-      }
-
-      inline btTransform convert(const ignition::math::Pose3d &pose)
-      {
-        btTransform tf;
-        tf.setOrigin(btVector3(convert(pose.Pos())));
-        tf.setRotation(btQuaternion(convert(pose.Rot())));
-
-        return tf;
-      }
-
-      // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-      // ---------------- Converting to ignition -------------------
-      inline ignition::math::Vector3d convert(const btVector3 &v)
-      {
-        ignition::math::Vector3d vec;
-        vec.X() = v[0];
-        vec.Y() = v[1];
-        vec.Z() = v[2];
-
-        return vec;
-      }
-
-      inline ignition::math::Matrix3d convert(const btMatrix3x3 &m)
-      {
-        ignition::math::Matrix3d matrix;
-        for (std::size_t i=0; i < 3; ++i)
-          for (std::size_t j=0; j < 3; ++j)
-            matrix(i, j) = m[i][j];
-
-        return matrix;
-      }
-
-      inline ignition::math::Quaterniond convert(const btQuaternion &q)
-      {
-        ignition::math::Quaterniond quat;
-        quat.W() = q.w();
-        quat.X() = q.x();
-        quat.Y() = q.y();
-        quat.Z() = q.z();
-
-        return quat;
-      }
-
-      inline ignition::math::Pose3d convert(const btTransform &tf)
-      {
-        ignition::math::Pose3d pose;
-        pose.Pos() = convert(btVector3(tf.getOrigin()));
-        pose.Rot() = convert(btQuaternion(tf.getRotation()));
-
-        return pose;
-      }
+      return btVector3(v[0], v[1], v[2]);
     }
+
+    inline btMatrix3x3 convert(const ignition::math::Matrix3d &m)
+    {
+      btMatrix3x3 matrix;
+      for (std::size_t i=0; i < 3; ++i)
+        for (std::size_t j=0; j < 3; ++j)
+          matrix[i][j] = m(i, j);
+
+      return matrix;
+    }
+
+    inline btQuaternion convert(const ignition::math::Quaterniond &q)
+    {
+      return btQuaternion(q.X(), q.Y(), q.Z(), q.W());
+    }
+
+    inline btTransform convert(const ignition::math::Pose3d &pose)
+    {
+      btTransform tf;
+      tf.setOrigin(btVector3(convert(pose.Pos())));
+      tf.setRotation(btQuaternion(convert(pose.Rot())));
+
+      return tf;
+    }
+
+    // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+    // ---------------- Converting to ignition -------------------
+    inline ignition::math::Vector3d convert(const btVector3 &v)
+    {
+      ignition::math::Vector3d vec;
+      vec.X() = v[0];
+      vec.Y() = v[1];
+      vec.Z() = v[2];
+
+      return vec;
+    }
+
+    inline ignition::math::Matrix3d convert(const btMatrix3x3 &m)
+    {
+      ignition::math::Matrix3d matrix;
+      for (std::size_t i=0; i < 3; ++i)
+        for (std::size_t j=0; j < 3; ++j)
+          matrix(i, j) = m[i][j];
+
+      return matrix;
+    }
+
+    inline ignition::math::Quaterniond convert(const btQuaternion &q)
+    {
+      ignition::math::Quaterniond quat;
+      quat.W() = q.w();
+      quat.X() = q.x();
+      quat.Y() = q.y();
+      quat.Z() = q.z();
+
+      return quat;
+    }
+
+    inline ignition::math::Pose3d convert(const btTransform &tf)
+    {
+      ignition::math::Pose3d pose;
+      pose.Pos() = convert(btVector3(tf.getOrigin()));
+      pose.Rot() = convert(btQuaternion(tf.getRotation()));
+
+      return pose;
+    }
+  }
 }
 
 #endif
