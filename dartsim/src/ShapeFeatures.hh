d92dc400652a177558308f007975c45d73c675ee
Steven Peters <scpeters@osrfoundation.org>
2019-02-27 21:40:37 +0000

    dartsim/src/ShapeFeatures.hh: add override to fix clang warning

--- dartsim/src/ShapeFeatures.hh
+++ dartsim/src/ShapeFeatures.hh
@@ -129,7 +129,7 @@ class ShapeFeatures :
 
   // ----- Collision Features -----
   public: AlignedBox3d GetAxisAlignedBoundingBox(
-              const Identity &_shapeID) const;
+              const Identity &_shapeID) const override;
 };
 
 }
