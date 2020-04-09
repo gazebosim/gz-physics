b64a1553d68f11f74c4d44ca2d6a26c60a39d680
Michael X. Grey <grey@openrobotics.org>
2020-04-09 22:13:34 +0800

    Attempting to reduce the symbols produced by InspectFeatures

--- include/ignition/physics/detail/InspectFeatures.hh
+++ include/ignition/physics/detail/InspectFeatures.hh
@@ -22,6 +22,7 @@
 #include <string>
 #include <tuple>
 
+#include <ignition/physics/FeatureList.hh>
 #include <ignition/plugin/PluginPtr.hh>
 
 namespace ignition
@@ -33,89 +34,103 @@ namespace ignition
       /////////////////////////////////////////////////
       /// \private This class is used to inspect what features are provided by
       /// a plugin. It implements the API of RequestEngine.
-      template <typename PolicyT, typename FeatureT, typename = void_t<> >
+
+      template <typename PolicyT, typename FeatureListT>
       struct InspectFeatures
       {
-        using Interface = typename FeatureT::template Implementation<PolicyT>;
+        using InterfaceTuple =
+          typename ExtractAPI<ImplementationSelector, FeatureListT>
+          ::template Bases<PolicyT>;
 
-        /// \brief Check that each feature is provided by the plugin.
-        template <typename PtrT>
-        static bool Verify(const PtrT &_pimpl)
+        struct S : std::tuple_size<InterfaceTuple> { };
+
+        using IndexSequence = std::make_index_sequence<S::value>;
+
+        template <typename Interface>
+        struct Impl
         {
-          return _pimpl && _pimpl->template HasInterface<Interface>();
-        }
-
-        template <typename LoaderT, typename ContainerT>
-        static void EraseIfMissing(
-            const LoaderT &_loader,
-            ContainerT &_plugins)
-        {
-          const auto acceptable =
-              _loader.template PluginsImplementing<Interface>();
-
-          std::set<std::string> unacceptable;
-          for (const std::string &p : _plugins)
+          template <typename PtrT>
+          static bool Verify(const PtrT &_pimpl)
           {
-            const auto it = std::find(acceptable.begin(), acceptable.end(), p);
-            if (it == acceptable.end())
-              unacceptable.insert(unacceptable.end(), p);
+            return _pimpl->template HasInterface<Interface>();
           }
 
-          for (const std::string &u : unacceptable)
-            _plugins.erase(u);
-        }
+          template <typename LoaderT, typename ContainerT>
+          static bool EraseIfMissing(
+              const LoaderT &_loader,
+              ContainerT &_plugins)
+          {
+            const auto acceptable =
+                _loader.template PluginsImplementing<Interface>();
 
-        template <typename PtrT>
-        static void MissingNames(const PtrT &_pimpl,
-                                 std::set<std::string> &_names)
+            std::set<std::string> unacceptable;
+            for (const std::string &p : _plugins)
+            {
+              const auto it = std::find(acceptable.begin(), acceptable.end(), p);
+              if (it == acceptable.end())
+                unacceptable.insert(unacceptable.end(), p);
+            }
+
+            for (const std::string &u : unacceptable)
+              _plugins.erase(u);
+
+            return !_plugins.empty();
+          }
+
+          template <typename PtrT>
+          static bool MissingNames(
+              const PtrT &_pimpl,
+              std::set<std::string> &_names)
+          {
+            if (_pimpl->template HasInterface<Interface>())
+            {
+              _names.insert(
+                    ignition::plugin::DemangleSymbol(
+                      typeid(Interface).name()));
+            }
+
+            return true;
+          }
+        };
+
+        template <std::size_t... I>
+        struct Impl<std::index_sequence<I...>>
         {
-          if (!_pimpl || !_pimpl->template HasInterface<Interface>())
-            _names.insert(typeid(FeatureT).name());
-        }
-      };
+          template <typename PtrT>
+          static bool Verify(const PtrT &_pimpl)
+          {
+            return _pimpl &&
+                (...&& Impl<std::tuple_element_t<I, InterfaceTuple>>
+                 ::Verify(_pimpl));
+          }
 
-      template <typename PolicyT>
-      struct InspectFeatures<PolicyT, void, void_t<> >
-      {
-        template <typename PtrT>
-        static bool Verify(const PtrT &/*_pimpl*/)
-        {
-          // This is just the terminal leaf of the inspection tree, so it must
-          // not falsify the verification.
-          return true;
-        }
+          template <typename LoaderT, typename ContainerT>
+          static void EraseIfMissing(
+              const LoaderT &_loader,
+              ContainerT &_plugins)
+          {
 
-        template <typename LoaderT, typename ContainerT>
-        static void EraseIfMissing(
-            const LoaderT &/*_loader*/,
-            ContainerT &/*_plugins*/)
-        {
-          // Do nothing, this is a terminal leaf
-        }
+            (...&& Impl<std::tuple_element_t<I, InterfaceTuple>>
+                ::EraseIfMissing(_loader, _plugins));
+          }
 
-        template <typename PtrT>
-        static void MissingNames(const PtrT &/*_pimpl*/,
-                                 std::set<std::string> &/*_names*/)
-        {
-          // Do nothing, this is a terminal leaf
-        }
-      };
+          template <typename PtrT>
+          static void MissingNames(
+              const PtrT &_pimpl,
+              std::set<std::string> &_names)
+          {
+            if (!_pimpl)
+              return;
 
-      /// \private Implementation of InspectFeatures.
-      template <typename PolicyT, typename FeatureListT>
-      struct InspectFeatures<PolicyT, FeatureListT,
-          void_t<typename FeatureListT::CurrentTupleEntry>>
-      {
-        using Branch1 = InspectFeatures<PolicyT,
-            typename FeatureListT::CurrentTupleEntry>;
-        using Branch2 = InspectFeatures<PolicyT,
-            typename GetNext<FeatureListT>::n>;
+            (...&& Impl<std::tuple_element_t<I, InterfaceTuple>>
+                ::MissingNames(_pimpl, _names));
+          }
+        };
 
-        /// \brief Check that each feature is provided by the plugin.
         template <typename PtrT>
         static bool Verify(const PtrT &_pimpl)
         {
-          return Branch1::Verify(_pimpl) && Branch2::Verify(_pimpl);
+          return Impl<IndexSequence>::Verify(_pimpl);
         }
 
         template <typename LoaderT, typename ContainerT>
@@ -123,18 +138,119 @@ namespace ignition
             const LoaderT &_loader,
             ContainerT &_plugins)
         {
-          Branch1::EraseIfMissing(_loader, _plugins);
-          Branch2::EraseIfMissing(_loader, _plugins);
+          Impl<IndexSequence>::EraseIfMissing(_loader, _plugins);
         }
 
         template <typename PtrT>
-        static void MissingNames(const PtrT &_pimpl,
-                                 std::set<std::string> &_names)
+        static void MissingNames(
+            const PtrT &_pimpl,
+            std::set<std::string> &_names)
         {
-          Branch1::MissingNames(_pimpl, _names);
-          Branch2::MissingNames(_pimpl, _names);
+          Impl<IndexSequence>::MissingNames(_pimpl, _names);
         }
       };
+//      template <typename PolicyT, typename FeatureT, typename = std::void_t<> >
+//      struct InspectFeatures
+//      {
+//        using Interface = typename FeatureT::template Implementation<PolicyT>;
+
+//        /// \brief Check that each feature is provided by the plugin.
+//        template <typename PtrT>
+//        static bool Verify(const PtrT &_pimpl)
+//        {
+//          return _pimpl && _pimpl->template HasInterface<Interface>();
+//        }
+
+//        template <typename LoaderT, typename ContainerT>
+//        static void EraseIfMissing(
+//            const LoaderT &_loader,
+//            ContainerT &_plugins)
+//        {
+//          const auto acceptable =
+//              _loader.template PluginsImplementing<Interface>();
+
+//          std::set<std::string> unacceptable;
+//          for (const std::string &p : _plugins)
+//          {
+//            const auto it = std::find(acceptable.begin(), acceptable.end(), p);
+//            if (it == acceptable.end())
+//              unacceptable.insert(unacceptable.end(), p);
+//          }
+
+//          for (const std::string &u : unacceptable)
+//            _plugins.erase(u);
+//        }
+
+//        template <typename PtrT>
+//        static void MissingNames(const PtrT &_pimpl,
+//                                 std::set<std::string> &_names)
+//        {
+//          if (!_pimpl || !_pimpl->template HasInterface<Interface>())
+//            _names.insert(typeid(FeatureT).name());
+//        }
+//      };
+
+//      template <typename PolicyT>
+//      struct InspectFeatures<PolicyT, void, std::void_t<> >
+//      {
+//        template <typename PtrT>
+//        static bool Verify(const PtrT &/*_pimpl*/)
+//        {
+//          // This is just the terminal leaf of the inspection tree, so it must
+//          // not falsify the verification.
+//          return true;
+//        }
+
+//        template <typename LoaderT, typename ContainerT>
+//        static void EraseIfMissing(
+//            const LoaderT &/*_loader*/,
+//            ContainerT &/*_plugins*/)
+//        {
+//          // Do nothing, this is a terminal leaf
+//        }
+
+//        template <typename PtrT>
+//        static void MissingNames(const PtrT &/*_pimpl*/,
+//                                 std::set<std::string> &/*_names*/)
+//        {
+//          // Do nothing, this is a terminal leaf
+//        }
+//      };
+
+//      /// \private Implementation of InspectFeatures.
+//      template <typename PolicyT, typename FeatureListT>
+//      struct InspectFeatures<PolicyT, FeatureListT,
+//          std::void_t<typename FeatureListT::CurrentTupleEntry>>
+//      {
+//        using Branch1 = InspectFeatures<PolicyT,
+//            typename FeatureListT::CurrentTupleEntry>;
+//        using Branch2 = InspectFeatures<PolicyT,
+//            typename GetNext<FeatureListT>::n>;
+
+//        /// \brief Check that each feature is provided by the plugin.
+//        template <typename PtrT>
+//        static bool Verify(const PtrT &_pimpl)
+//        {
+//          return Branch1::Verify(_pimpl) && Branch2::Verify(_pimpl);
+//        }
+
+//        template <typename LoaderT, typename ContainerT>
+//        static void EraseIfMissing(
+//            const LoaderT &_loader,
+//            ContainerT &_plugins)
+//        {
+//          Branch1::EraseIfMissing(_loader, _plugins);
+//          Branch2::EraseIfMissing(_loader, _plugins);
+//        }
+
+//        template <typename PtrT>
+//        static void MissingNames(const PtrT &_pimpl,
+//                                 std::set<std::string> &_names)
+//        {
+//          Branch1::MissingNames(_pimpl, _names);
+//          Branch2::MissingNames(_pimpl, _names);
+//        }
+//      };
     }
   }
 }
