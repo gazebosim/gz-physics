c9bd783fe88b57424a3347f1597a4af1a66c6626
Jose Tomas Lorente <jtlorente@ekumenlabs.com>
2020-12-01 17:10:08 -0300

    [bullet] Basic Features (#172)
    * Fixed gravity, pose, and corrected some auto variables
    
    Signed-off-by: Tomas Lorente <jtlorente@ekumenlabs.com>
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

--- bullet/src/SDFFeatures.cc
+++ bullet/src/SDFFeatures.cc
@@ -26,12 +26,12 @@ Identity SDFFeatures::ConstructSdfModel(
 {
   // Read sdf params
   const std::string name = _sdfModel.Name();
-  const auto pose = _sdfModel.RawPose();
+  const math::Pose3d pose = _sdfModel.RawPose();
   const bool isStatic = _sdfModel.Static();
   // const bool selfCollide = _sdfModel.SelfCollide();
 
   // const auto &world = this->worlds.at(_worldID)->world;
-  const auto modelIdentity = this->AddModel({name, _worldID, isStatic});
+  const auto modelIdentity = this->AddModel({name, _worldID, isStatic, pose});
 
   // Build links
   for (std::size_t i = 0; i < _sdfModel.LinkCount(); ++i)
@@ -59,8 +59,8 @@ Identity SDFFeatures::ConstructSdfLink(
   const btVector3 linkInertiaDiag =
       convertVec(ignition::math::eigen3::convert(diagonalMoments));
 
-  // (TO-DO: do those calculation need to take into account in some way the base model?)
-  const auto poseIsometry = ignition::math::eigen3::convert(pose);
+  math::Pose3d base_pose = this->models.at(_modelID)->pose;
+  const auto poseIsometry = ignition::math::eigen3::convert(base_pose * pose);
   const auto poseTranslation = poseIsometry.translation();
   const auto poseLinear = poseIsometry.linear();
   btTransform baseTransform;
@@ -68,9 +68,11 @@ Identity SDFFeatures::ConstructSdfLink(
   baseTransform.setBasis(convertMat(poseLinear));
 
   // Create link
-  // (TO-DO: do we want to use MotionState?)
-  // First zero is motionState, the second one is the colision shape
-  btRigidBody* body = new btRigidBody(mass, 0, 0, linkInertiaDiag);
+  // (TO-DO: do we want to use MotionState?) 2nd part: Do motion state use the same transformation?
+  btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(0.), btScalar(0.), btScalar(0.))); // Collision 0 until we have collision features
+  btDefaultMotionState* myMotionState = new btDefaultMotionState(baseTransform);
+  btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, groundShape, linkInertiaDiag);
+  btRigidBody* body = new btRigidBody(rbInfo);
   body->setWorldTransform(baseTransform);
 
   // Add it to the internal world:
