c59f22f5f2b1b01fd00cd6d853e0577ecbe9a8f1
Tomas Lorente <jtlorente@ekumenlabs.com>
2020-12-23 15:06:46 -0300

    added joint position methods

--- bullet/src/SDFFeatures.cc
+++ bullet/src/SDFFeatures.cc
@@ -230,7 +230,6 @@ Identity SDFFeatures::ConstructSdfCollision(
     const auto &body = linkInfo->link;
     const auto &modelID = linkInfo->model;
     const auto &modelInfo = this->models.at(modelID);
-    const auto &link = linkInfo->link;
     const auto &world = this->worlds.at(modelInfo->world)->world;
     const auto &mass = linkInfo->mass;
     const auto &inertia = linkInfo->inertia;
@@ -249,7 +248,7 @@ Identity SDFFeatures::ConstructSdfCollision(
     baseTransform.setOrigin(convertVec(poseTranslation));
     baseTransform.setBasis(convertMat(poseLinear));
 
-    shape->setMargin(btScalar(0.0001));
+    // shape->setMargin(btScalar(0.0001));
 
     btDefaultMotionState* myMotionState = new btDefaultMotionState(baseTransform);
     btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, shape, inertia);
@@ -395,7 +394,9 @@ Identity SDFFeatures::ConstructSdfJoint(
   joint->enableFeedback(true);
 
   // Generate an identity for it and return it
-  return this->AddJoint({_sdfJoint.Name(), joint, childId, parentId, static_cast<int>(type)});
+  auto identity = this->AddJoint({_sdfJoint.Name(), joint, childId, parentId, static_cast<int>(type)});
+  ignerr << "Created joint " << identity.id << std::endl;
+  return identity;
 }
 
 /////////////////////////////////////////////////
