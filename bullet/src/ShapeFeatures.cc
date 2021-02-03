e11cafbc2b696b47d32643996259d240170f4540
Jorge Perez <jjperez@ekumenlabs.com>
2021-02-03 13:55:04 -0500

    Add some reformat and copyright notices
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

--- bullet/src/ShapeFeatures.cc
+++ bullet/src/ShapeFeatures.cc
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 #include "ShapeFeatures.hh"
 #include <BulletCollision/Gimpact/btGImpactShape.h>
 #include <BulletCollision/Gimpact/btGImpactCollisionAlgorithm.h>
@@ -74,11 +91,12 @@ Identity ShapeFeatures::AttachMeshShape(
 
   // gimpactMeshShape->setMargin(btScalar(0.001));
 
-  dynamic_cast<btCompoundShape *>(body->getCollisionShape())->addChildShape(baseTransform, gimpactMeshShape);
+  dynamic_cast<btCompoundShape *>(
+    body->getCollisionShape())->addChildShape(baseTransform, gimpactMeshShape);
   btGImpactCollisionAlgorithm::registerAlgorithm(worldInfo->dispatcher);
 
-  auto identity = this->AddCollision({_name, gimpactMeshShape, _linkID,
-                             modelID, ignition::math::eigen3::convert(_pose)});
+  auto identity = this->AddCollision(
+    {_name, gimpactMeshShape, _linkID, modelID, ignition::math::eigen3::convert(_pose)});
   return identity;
 
 }
