b2a8bc4c4ec9a75b65ec05c954f3f8a9d9bff255
Alejandro Hernández Cordero <ahcorde@gmail.com>
2022-09-12 22:01:28 +0200

    Added world features common tests (#414)
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- dartsim/src/WorldFeatures_TEST.cc
+++ dartsim/src/WorldFeatures_TEST.cc
@@ -104,7 +104,8 @@ TestWorldPtr LoadWorld(
 //////////////////////////////////////////////////
 TEST_F(WorldFeaturesFixture, CollisionDetector)
 {
-  auto world = LoadWorld(this->engine, TEST_WORLD_DIR "/empty.sdf");
+  auto world = LoadWorld(this->engine,
+    gz::common::joinPaths(TEST_WORLD_DIR, "empty.sdf"));
   EXPECT_EQ("ode", world->GetCollisionDetector());
 
   world->SetCollisionDetector("banana");
@@ -124,79 +125,11 @@ TEST_F(WorldFeaturesFixture, CollisionDetector)
 }
 
 //////////////////////////////////////////////////
-TEST_F(WorldFeaturesFixture, Gravity)
-{
-  auto world = LoadWorld(this->engine, TEST_WORLD_DIR "/falling.world");
-  ASSERT_NE(nullptr, world);
-
-  // Check default gravity value
-  AssertVectorApprox vectorPredicate10(1e-10);
-  EXPECT_PRED_FORMAT2(vectorPredicate10, Eigen::Vector3d(0, 0, -9.8),
-                      world->GetGravity());
-
-  auto model = world->GetModel("sphere");
-  ASSERT_NE(nullptr, model);
-
-  auto link = model->GetLink(0);
-  ASSERT_NE(nullptr, link);
-
-  // initial link pose
-  const Eigen::Vector3d initialLinkPosition(0, 0, 2);
-  {
-    Eigen::Vector3d pos = link->FrameDataRelativeToWorld().pose.translation();
-    EXPECT_PRED_FORMAT2(vectorPredicate10,
-                        initialLinkPosition,
-                        pos);
-  }
-
-  auto linkFrameID = link->GetFrameID();
-
-  // Get default gravity in link frame, which is pitched by pi/4
-  EXPECT_PRED_FORMAT2(vectorPredicate10,
-                      Eigen::Vector3d(6.92964645563, 0, -6.92964645563),
-                      world->GetGravity(linkFrameID));
-
-  // set gravity along X axis of linked frame, which is pitched by pi/4
-  world->SetGravity(Eigen::Vector3d(1.4142135624, 0, 0), linkFrameID);
-
-  EXPECT_PRED_FORMAT2(vectorPredicate10,
-                      Eigen::Vector3d(1, 0, -1),
-                      world->GetGravity());
-
-  // test other SetGravity API
-  // set gravity along Z axis of linked frame, which is pitched by pi/4
-  physics::RelativeForce3d relativeGravity(
-      linkFrameID, Eigen::Vector3d(0, 0, 1.4142135624));
-  world->SetGravity(relativeGravity);
-
-  EXPECT_PRED_FORMAT2(vectorPredicate10,
-                      Eigen::Vector3d(1, 0, 1),
-                      world->GetGravity());
-
-  // Confirm that changed gravity direction affects pose of link
-  gz::physics::ForwardStep::Input input;
-  gz::physics::ForwardStep::State state;
-  gz::physics::ForwardStep::Output output;
-
-  const size_t numSteps = 1000;
-  for (size_t i = 0; i < numSteps; ++i)
-  {
-    world->Step(output, state, input);
-  }
-
-  AssertVectorApprox vectorPredicate3(1e-3);
-  {
-    Eigen::Vector3d pos = link->FrameDataRelativeToWorld().pose.translation();
-    EXPECT_PRED_FORMAT2(vectorPredicate3,
-                        Eigen::Vector3d(0.5, 0, 2.5),
-                        pos);
-  }
-}
-
-//////////////////////////////////////////////////
 TEST_F(WorldFeaturesFixture, Solver)
 {
-  auto world = LoadWorld(this->engine, TEST_WORLD_DIR "/empty.sdf");
+  auto world = LoadWorld(this->engine,
+    gz::common::joinPaths(TEST_WORLD_DIR, "empty.sdf"));
+
   EXPECT_EQ("DantzigBoxedLcpSolver", world->GetSolver());
 
   world->SetSolver("banana");
