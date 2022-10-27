5d060059045d1562cdafdb8a67dab0ffb15ca3a7
ahcorde <ahcorde@gmail.com>
2022-10-27 15:05:36 +0200

    Removed GetCollision API it's redundant
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- bullet-featherstone/src/SDFFeatures.hh
+++ bullet-featherstone/src/SDFFeatures.hh
@@ -59,10 +59,6 @@ class SDFFeatures :
   private: Identity ConstructSdfCollision(
       const Identity &_linkID,
       const ::sdf::Collision &_collision) override;
-
-  private: Identity GetCollision(
-      const Identity &_linkID,
-      const std::string &_collisionName) override;
 };
 
 }  // namespace bullet_featherstone
