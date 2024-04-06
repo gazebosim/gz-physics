d175730c06571bd47e7773a652f196a13cc10a55
Ian Chen <ichen@openrobotics.org>
2024-04-06 00:48:26 +0000

    fix build
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- bullet-featherstone/src/plugin.cc
+++ bullet-featherstone/src/plugin.cc
@@ -40,7 +40,9 @@ struct BulletFeatures : FeatureList <
   EntityManagementFeatureList,
   SimulationFeatureList,
   FreeGroupFeatureList,
+#if BT_BULLET_VERSION >= 307
   KinematicFeatureList,
+#endif
   KinematicsFeatureList,
   LinkFeatureList,
   SDFFeatureList,
@@ -55,7 +57,9 @@ class Plugin :
     public virtual EntityManagementFeatures,
     public virtual SimulationFeatures,
     public virtual FreeGroupFeatures,
+#if BT_BULLET_VERSION >= 307
     public virtual KinematicFeatures,
+#endif
     public virtual KinematicsFeatures,
     public virtual LinkFeatures,
     public virtual SDFFeatures,
