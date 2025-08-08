aa3d940b4a91ae31db9e0db3e652f22467f4e347
Jose Mayoral <jocamaba1989@gmail.com>
2025-08-08 14:34:21 +0000

    typo on convert function
    Signed-off-by: Jose Mayoral <jocamaba1989@gmail.com>

--- test/common_test/kinematic_features.cc
+++ test/common_test/kinematic_features.cc
@@ -342,7 +342,7 @@ TEST_F(SetKinematicTestFeaturesList, SetFalseKinematic)
  
     EXPECT_NEAR(expected_z,
                 frameData.pose.translation().z(), 1e-2);
-    EXPECT_EQ(gz::math::eigen::convert(expected_v),
+    EXPECT_EQ(gz::math::eigen3::convert(expected_v),
               gz::math::eigen3::convert(frameData.linearVelocity));
     EXPECT_EQ(gz::math::Vector3d::Zero,
               gz::math::eigen3::convert(frameData.angularVelocity));
