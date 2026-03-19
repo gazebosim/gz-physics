94c811401e42d92c6b5b752853b1d77b6fface8b
Ian Chen <ichen@openrobotics.org>
2026-03-19 13:59:12 -0700

    bullet-featherstone: Fix contact force and depth data (#890)
    Signed-off-by: Ian Chen <iche@intrinsic.ai>
    (cherry picked from commit 913658adf0f919a808b842356368a7600e12eb31)

--- bullet-featherstone/src/SimulationFeatures.cc
+++ bullet-featherstone/src/SimulationFeatures.cc
@@ -289,15 +289,15 @@ SimulationFeatures::GetContactsFromLastStep(const Identity &_worldID) const
 
       CompositeData extraData;
 
-      // Add normal, depth and wrench to extraData.
+      // Add normal, depth and force to extraData.
       auto& extraContactData =
         extraData.Get<SimulationFeatures::ExtraContactData>();
+
+      const Eigen::Vector3d normal = convert(pt.m_normalWorldOnB);
       extraContactData.force =
-        convert(btVector3(pt.m_appliedImpulse,
-                          pt.m_appliedImpulse,
-                          pt.m_appliedImpulse));
-      extraContactData.normal = convert(pt.m_normalWorldOnB);
-      extraContactData.depth = pt.getDistance();
+          normal * (pt.m_appliedImpulse / world->stepSize);
+      extraContactData.normal = normal;
+      extraContactData.depth = -pt.getDistance();
 
       outContacts.push_back(SimulationFeatures::ContactInternal {
         this->GenerateIdentity(collision0ID, this->collisions.at(collision0ID)),
