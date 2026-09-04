2a9a785a75e79feb8cbbc333662d93088a684c9e
Addisu Z. Taddese <addisuzt@intrinsic.ai>
2026-09-04 15:35:42 -0500

    Add failing test showing sequence of SetForce(0) and GetForce
    Signed-off-by: Addisu Z. Taddese <addisuzt@intrinsic.ai>

--- test/common_test/joint_basic_features.cc
+++ test/common_test/joint_basic_features.cc
@@ -301,6 +301,11 @@ TEST_F(BasicJointFeaturesTest, GetSetForceAccel)
     {
       EXPECT_DOUBLE_EQ(kForceCmd * 10, this->joint->GetForce(0));
     }
+    // This should also be true if the commanded force is 0.
+    this->Step();
+    EXPECT_GT(this->joint->GetForce(0), 0);
+    this->joint->SetForce(0, 0);
+    EXPECT_DOUBLE_EQ(0, this->joint->GetForce(0));
   }
 }
 
